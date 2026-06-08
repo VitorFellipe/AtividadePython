8eb83ea96b8bb9252475b05fbe0a87983078648b
VitorFellipe <githubv8@gmail.com>
2026-06-07 21:34:12 -0300

    Add files via upload
    Projeto Professor Diego de orientação a objeto!

---
+++ ProjetoOO em c#.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    public class Funcionario
+    {
+        private string nome;
+        private string matricula;
+        private double salario;
+
+        public virtual double CalcularSalario()
+        {
+            return salario;
+        }
+    
+    public class clt: Funcionario
+        {
+            public double salariofixo { get; set;}
+
+            public override double CalcularSalario()
+            {
+                return salario;
+            }
+        }
+        public class vendedor : Funcionario
+        {
+            public double salariofixo { get; set; }
+            public double comissãovendasporcentagem10 {get; set;}
+
+            public override double CalcularSalario()
+            {
+                return salario + (comissãovendasporcentagem10 * 0.10);
+            }
+        }
+        public class gerente : Funcionario
+        {
+            public double salariofixo { get; set;}
+            public double bonuxfixo1500reais { get; set;}
+
+            public override double CalcularSalario()
+            {
+                return salario + 1500;
+            }
+        }
+        public double Salario
+        {
+            get
+            {
+                return salario;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    salario = value;
+                }
+            }
+        }
+    }
+}
